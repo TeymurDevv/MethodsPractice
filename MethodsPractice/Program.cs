38c15b4834be1a239034376f3ab146cf4fef0670
TeymurDevv <transmissiyaservis@gmail.com>
2023-10-28 23:23:35 +0400

    Task2 Completed

--- MethodsPractice/Program.cs
+++ MethodsPractice/Program.cs
@@ -10,3 +10,18 @@ string Divide(int n)
 string divideResult = Divide(21);
 Console.WriteLine(divideResult);
 #endregion
+
+#region task2
+string evenSum(int n, int m)
+{
+    if (n%2==0 && m%2==0)
+    {
+        int result = n + m;
+        return $"{n} ve {m} ededleri cutdur ve iki reqemin cemi {result} olur";
+    }
+    return $"{n} ve {m} ededlerinden her hansi biri cut deyil";
+}
+string evenSumResult = evenSum(2, 5);
+Console.WriteLine(evenSumResult);
+
+#endregion
