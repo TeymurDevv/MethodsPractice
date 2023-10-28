c436d029f7b5c6720369e4cf20efd5f0797fe731
TeymurDevv <transmissiyaservis@gmail.com>
2023-10-28 23:09:34 +0400

    Task1 Completed

---
+++ MethodsPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿#region task1
+string Divide(int n)
+{
+    if (n%3==0 && n%7==0)
+    {
+        return $"{n} Ededi hem 3 e hem 7 ye bolunur";
+    }
+    else
+    {
+        return $"{n} Ededi hem 3 e hem 7 ye bolunmur";
+    }
+}
+string divideResult = Divide(28);
+Console.WriteLine(divideResult);
+#endregion
